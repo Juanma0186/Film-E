2574ce59958716643c1f4c78fa213706a4bffd62
Juanma0186 <juanma0186@gmail.com>
2023-11-20 03:55:57 +0100

    Add estructura de film-e 2

--- film-project/vite.config.js
+++ film-project/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
-    open: '/src/login.html',
+    open: "/src/login.html",
   },
-})
+  build: {
+    outDir: "../dist",
+    assetsDir: "./",
+  },
+});
