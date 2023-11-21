ff395f251814f6a6d183b41f71548ddb8ea6aab5
Juanma0186 <juanma0186@gmail.com>
2023-11-21 03:27:45 +0100

    ADD animación texto y pelis populares

--- Film-E_v2/src/script/detallePelicula.js
+++ Film-E_v2/src/script/detallePelicula.js
@@ -14,18 +14,14 @@ fetch(MOVIE_API_URL)
     const movieDetails = document.getElementById("movieDetails");
     movieDetails.innerHTML = `
         <div class="flex flex-col md:flex-row">
-        <img class="w-full md:w-[400px] h-[400px] object-containt rounded-lg mb-5 md:mb-0 md:mr-8" src="${IMAGE_URL}${
-      movie.poster_path
-    }" alt="${movie.title}">
+        <img class="w-full md:w-[400px] h-[400px] object-containt rounded-lg mb-5 md:mb-0 md:mr-8" src="${IMAGE_URL}${movie.poster_path}" alt="${movie.title}">
         <div class="text-azul-100">
             <h2 class="text-3xl font-bold mb-5">${movie.title}</h2>
             <p class="mb-4">${movie.overview}</p>
             <p class="mb-4">Fecha de estreno: ${movie.release_date}</p>
             <p class="mb-4">Duración: ${movie.runtime} minutos</p>
             <p class="mb-4">Puntuación: ${movie.vote_average}</p>
-            <p class="mb-4">Géneros: ${movie.genres
-              .map((genre) => genre.name)
-              .join(", ")}</p>
+            <p class="mb-4">Géneros: ${movie.genres.map((genre) => genre.name).join(", ")}</p>
             <p class="mb-4">Idioma original: ${movie.original_language}</p>
             <button id="trailerButton" class="px-4 py-2  font-bold rounded bg-azul-500 text-blanco-700 hover:bg-azul-300">
             Ver tráiler
