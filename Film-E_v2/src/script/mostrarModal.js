1ca861fa2dfdc1c415fc4a9a1d28bba3a0dd2a94
Juanma0186 <juanma0186@gmail.com>
2023-11-22 19:40:51 +0100

    FIXED modals y ADD autoplay al trailer

--- Film-E_v2/src/script/mostrarModal.js
+++ Film-E_v2/src/script/mostrarModal.js
@@ -2,4 +2,5 @@ export function mostrarModal(imageSrc) {
   document.getElementById("modalImage").src = imageSrc;
   document.getElementById("imageModal").alt = imageSrc;
   document.getElementById("imageModal").classList.remove("hidden");
+  document.getElementById("imageModal").classList.add("flex");
 }
--- Film-E_v2/src/script/verTrailer.js
+++ Film-E_v2/src/script/verTrailer.js
@@ -1,17 +1,23 @@
 export function verTrailer(movieTitle) {
-    const YOUTUBE_API_KEY = "AIzaSyBa4JfbgovmH0oBE3jHBJSfqTxlBCP0sZM";
-    const YOUTUBE_API_URL = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${movieTitle} tráiler&type=video&key=${YOUTUBE_API_KEY}`;
-  
-    fetch(YOUTUBE_API_URL)
-      .then((response) => response.json())
-      .then((data) => {
-        const videoId = data.items[0].id.videoId;
-        const trailerContainer = document.getElementById("trailerContainer");
-        trailerContainer.innerHTML = `
-                      <iframe width="100%" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                  `;
-        document.getElementById("trailerModal").classList.remove("hidden");
-      })
-      .catch((error) => console.error("Error:", error));
-  }
-  
+  const YOUTUBE_API_KEY = "AIzaSyBa4JfbgovmH0oBE3jHBJSfqTxlBCP0sZM";
+  const YOUTUBE_API_URL = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${movieTitle} tráiler&type=video&key=${YOUTUBE_API_KEY}`;
+
+  fetch(YOUTUBE_API_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      const videoId = data.items[0].id.videoId;
+      const trailerContainer = document.getElementById("trailerContainer");
+      trailerContainer.innerHTML = `
+        <iframe
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/${videoId}?autoplay=1"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+        ></iframe>                `;
+      document.getElementById("trailerModal").classList.remove("hidden");
+      document.getElementById("trailerModal").classList.add("flex");
+    })
+    .catch((error) => console.error("Error:", error));
+}
