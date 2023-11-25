de12c1e12238cd7e0630b749bf542871fa8bd219
Juanma0186 <juanma0186@gmail.com>
2023-11-25 05:18:25 +0100

    FIXED mdoo oscuro sin flashazo

--- Film-E_v2/src/script/modoOscuro.js
+++ Film-E_v2/src/script/modoOscuro.js
@@ -2,23 +2,14 @@ export function toggleDarkMode() {
   const btnSwitch = document.querySelector("#switch");
 
   btnSwitch.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
+    document.documentElement.classList.toggle("dark");
     btnSwitch.classList.toggle("active");
 
     // Guardamos las preferencias del usuario en localStorage
-    if (document.body.classList.contains("dark")) {
-      localStorage.setItem("dark-mode", "true");
+    if (document.documentElement.classList.contains("dark")) {
+      localStorage.setItem("dark-mode", "dark");
     } else {
-      localStorage.setItem("dark-mode", "false");
+      localStorage.setItem("dark-mode", "light");
     }
   });
-
-  // Obtenemos las preferencias del usuario desde localStorage
-  if (localStorage.getItem("dark-mode") === "true") {
-    document.body.classList.add("dark");
-    btnSwitch.classList.add("active");
-  } else {
-    document.body.classList.remove("dark");
-    btnSwitch.classList.remove("active");
-  }
 }
