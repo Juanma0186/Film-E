4a64ada9132dbebb0d126de59bfb1a9f06dfdac5
Juanma0186 <juanma0186@gmail.com>
2023-11-27 05:13:01 +0100

    Estilado buscador

--- Film-E_v2/src/script/buscador.js
+++ Film-E_v2/src/script/buscador.js
@@ -1,4 +1,5 @@
 import { SEARCH_API_URL, SEARCH_ALL } from "./config.js";
+import * as utils from "./utilities.js";
 import $ from "jquery";
 const searchInput = $("#search-input");
 const searchResults = $("#search-results");
@@ -19,39 +20,48 @@ searchInput.on("input", debounce(() => {
 
       if (results.length === 0) {
         const messageElement = $("<p>").text("No se encontraron resultados de búsqueda.");
+        messageElement.addClass("text-black dark:text-white font-bold");
 
         searchResults.append(messageElement);
       } else {
         results.forEach((result) => {
           console.log(result);
-          const resultElement = $("<div>").addClass("cursor-pointer");
+          /* Creamos el elemento a que contendrá el póster y el título */
+          const resultElement = $("<a>").addClass("flex gap-4 items-center w-fit p-2 bg-blanco-500 dark:bg-gris-100 rounded-lg hover:bg-blanco-400 transition-all duration-200 ease-linear cursor-pointer max-w-[600px]");
 
-          const mediaType = result.media_type === "movie" ? $("<p>").text("Película") : $("<p>").text("Serie");
-          const titleElement = $("<h2>").text(result.title || result.name);
+          /* Le asignamos un href en función de si es peli o serieS */
+          if (result.media_type === "movie") {
+            resultElement.attr("href", `detallePelicula.html?id=${result.id}`);
+          } else {
+            resultElement.attr("href", `detalleSerie.html?id=${result.id}`);
+          }
 
-          const imageElement = $("<img>").attr("src", `https://image.tmdb.org/t/p/w500${result.poster_path}`);
-          imageElement.attr("alt", result.title);
-          imageElement.addClass("w-[100px]", "h-[150px]", "object-cover", "rounded", "my-2", "text-center");
+          const mediaType = result.media_type === "movie" ? "Película" : "Serie";
+          const titleElement = result.title || result.name;
 
-          imageElement.on("error", () => {
-            if (result.media_type === "movie") {
-              imageElement.attr("src", "img/default-peli.webp");
-            } else {
-              imageElement.attr("src", "img/default-serie.webp");
-            }
-          });
+          const average = utils.roundAverage(result.vote_average);
+          console.log(result);
+          const formattedDate = utils.formatDate(result.release_date || result.first_air_date);
+          resultElement.html(`
+            <div class="h-full">
+              <img class="rounded-lg w-[96px] lg:w-[144px] h-[144px] lg:h-[216px] object-cover" alt="${mediaType} ${titleElement}" src="${result.poster_path ? `https://image.tmdb.org/t/p/w500${result.poster_path}` : (result.media_type === "movie" ? "img/default-peli.webp" : "img/default-serie.webp")}" loading="lazy" />
+            </div >
+            <div class="p-4 flex flex-col justify-start text-left gap-2 flex-[2]">
+              <p class="text-lg text-black dark:text-blanco-500 font-poppins">${titleElement}</p>
+              <p class="text-md text-gris-300 dark:text-gris-500">${formattedDate}</p>
+              <div class="relative w-[50px] lg:w-[60px]">
+                  <svg viewBox="0 0 36 36" class="max-w-full ${average >= 70 ? "green" : (average <= 45 ? "red" : "yellow")} max-w-[50px] lg:max-w-[60px]">
+                  <path class="circle" stroke-dasharray="${average}, 100" d="M18 2.0845
+                  a 15.9155 15.9155 0 0 1 0 31.831
+                  a 15.9155 15.9155 0 0 1 0 -31.831"></path>
+                  <text x="18" y="22" class="percentage">${average}</text>
+                </svg>
+              </div>
+            </div >
+          `
 
-          resultElement.append(mediaType);
-          resultElement.append(titleElement);
-          resultElement.append(imageElement);
+          );
 
-          resultElement.on("click", () => {
-            if (result.media_type === "movie") {
-              window.location.href = `detallePelicula.html?id=${result.id}`;
-            } else {
-              window.location.href = `detalleSerie.html?id=${result.id}`;
-            }
-          });
           searchResults.append(resultElement);
         });
       }
