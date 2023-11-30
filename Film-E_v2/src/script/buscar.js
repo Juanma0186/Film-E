ecf73e05904282bba2c37af16621f7e9ac298fa4
angel142330@gmail.com <angel142330@gmail.es>
2023-11-30 23:29:09 +0100

    buscar y filtrar peli V2

--- Film-E_v2/src/script/buscar.js
+++ Film-E_v2/src/script/buscar.js
@@ -1,70 +1,91 @@
-let resultados = [];
+import { API_KEY } from "./config.js";
 
-document.getElementById("formulario").addEventListener("submit", function (event) {
-  event.preventDefault();
-  const titulo = document.getElementById("titulo").value;
-  const tipo = document.getElementById("tipo").value;
-  buscar(titulo, tipo);
-});
+const apiBaseUrl = "https://api.themoviedb.org/3";
+let currentPage = 1;
+let movies = [];
+let selectedGenre = null;
+const genres = {};
 
-document.getElementById("ordenFecha").addEventListener("change", function () {
-  ordenarResultados(this.value);
-});
+function searchMovies() {
+  const sortBy = document.getElementById("sort_by").value || "popularity.desc";
+  let url = `${apiBaseUrl}/discover/movie?api_key=${API_KEY}&language=es-ES&sort_by=${sortBy}&page=${currentPage}&primary_release_date.gte=1980-01-01&primary_release_date.lte=2023-12-31`;
+  if (selectedGenre) {
+    url += `&with_genres=${selectedGenre}`;
+  }
 
-document.getElementById("ordenPopularidad").addEventListener("change", function () {
-  ordenarResultados(this.value);
-});
-
-document.getElementById("ordenTitulo").addEventListener("change", function () {
-  ordenarResultados(this.value);
-});
-
-async function buscar(titulo, tipo) {
-  const response = await fetch(`https://api.themoviedb.org/3/search/multi?api_key=a35eb9b2a0da4da2cd02766b7d42ed24&query=${titulo}`);
-  const data = await response.json();
-  resultados = data.results.filter(resultado => ["movie", "tv"].includes(resultado.media_type) && (tipo ? resultado.media_type === tipo : true));
-  mostrarResultados(resultados);
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      movies = data.results;
+      renderMovies();
+    })
+    .catch(error => {
+      console.error(error);
+    });
 }
 
-function mostrarResultados(resultados) {
-  const contenedor = document.getElementById("resultados");
-  contenedor.innerHTML = "";
-  if (resultados.length === 0) {
-    contenedor.innerHTML = "<p class='text-lg text-gris-300 font-bold' >Ninguna película o serie coincide con tu búsqueda.</p>";
-    return;
-  }
-  for (const resultado of resultados) {
-    console.log(resultado);
-    const movie = document.createElement("a");
-    movie.href = resultado.release_date
-      ? `./detallePelicula.html?id=${resultado.id}`
-      : `./detalleSerie.html?id=${resultado.id}`;
-    movie.className = "p-4 bg-white rounded shadow resultado";
-    movie.innerHTML = `
-            <h2 class="text-lg font-bold mb-2 ">${resultado.title || resultado.name}</h2>
-            <img src="${resultado.poster_path ? `https://image.tmdb.org/t/p/w500${resultado.poster_path}` : "img/default.png"}" alt="${resultado.title || resultado.name}" class="w-full rounded mb-2">
-            <p><strong>Tipo:</strong> ${resultado.media_type === "movie" ? "Película" : "Serie"}</p>
-            <p><strong>Fecha de lanzamiento:</strong> ${resultado.release_date || resultado.first_air_date}</p>
-            <p><strong>Popularidad:</strong> ${resultado.popularity}</p>
+function renderMovies() {
+  const resultsDiv = document.getElementById("results");
+  resultsDiv.innerHTML = "";
+
+  for (const movie of movies) {
+    if (movie.poster_path && movie.release_date) {
+      const movieDiv = document.createElement("div");
+      movieDiv.classList.add("bg-white", "p-4", "rounded", "shadow");
+      movieDiv.innerHTML = `
+            <h2 class="text-xl mb-2">${movie.title}</h2>
+            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" class="w-full mb-2 rounded">
+            <p>Fecha de lanzamiento: ${movie.release_date}</p>
+            <p>Valoración: ${movie.vote_average}</p>
+            <p>Géneros: ${movie.genre_ids.map(id => genres[id]).join(", ")}</p>
         `;
-    contenedor.appendChild(movie);
+      resultsDiv.appendChild(movieDiv);
+    }
   }
 }
 
-function ordenarResultados(orden) {
-  resultados.sort((a, b) => {
-    if (orden.includes("release_date")) {
-      const fechaA = new Date(a.release_date || a.first_air_date);
-      const fechaB = new Date(b.release_date || b.first_air_date);
-      return orden.includes("asc") ? fechaA - fechaB : fechaB - fechaA;
-    } else if (orden.includes("popularity")) {
-      return orden.includes("asc") ? a.popularity - b.popularity : b.popularity - a.popularity;
-    } else if (orden.includes("title")) {
-      const tituloA = a.title || a.name;
-      const tituloB = b.title || b.name;
-      return orden.includes("asc") ? tituloA.localeCompare(tituloB) : tituloB.localeCompare(tituloA);
-    }
-    return 0;
-  });
-  mostrarResultados(resultados);
+function nextPage() {
+  currentPage++;
+  searchMovies();
 }
+
+function previousPage() {
+  if (currentPage > 1) {
+    currentPage--;
+    searchMovies();
+  }
+}
+
+function selectGenre(genreId) {
+  selectedGenre = genreId;
+  searchMovies();
+}
+
+function loadGenres() {
+  const url = `${apiBaseUrl}/genre/movie/list?api_key=${API_KEY}&language=es-ES`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const genresDiv = document.getElementById("genres");
+      genresDiv.innerHTML = "";
+
+      for (const genre of data.genres) {
+        genres[genre.id] = genre.name;
+        const button = document.createElement("button");
+        button.classList.add("px-4", "py-2", "bg-red-500", "text-white", "rounded-full", "mr-2", "mb-2");
+        button.textContent = genre.name;
+        button.onclick = () => selectGenre(genre.id);
+        genresDiv.appendChild(button);
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+// Llama a la función de búsqueda y carga los géneros cuando se carga la página
+window.onload = function () {
+  searchMovies();
+  loadGenres();
+};
