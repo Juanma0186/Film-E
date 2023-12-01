4d3ec4db8f0c47a6d34fe595cf68fda9282e1b36
Juanma0186 <juanma0186@gmail.com>
2023-12-01 04:08:01 +0100

    DELETE reseñas y back button

--- Film-E_v2/src/script/verDetalleSerie.js
+++ Film-E_v2/src/script/verDetalleSerie.js
@@ -3,15 +3,9 @@ import { verReparto } from "./verReparto.js";
 import { verDetalle } from "./verDetallePelicula.js";
 import { toTop } from "./toTop.js";
 import { navbar } from "./navbar.js";
-import { verReseñas } from "./verReseñas.js";
 
 verDetalle(SERIE_API_URL);
 verReparto(SERIE_CAST_API_URL, "castList");
-verReseñas(TV_REVIEW_API_URL, "reviewsList");
-
-document.getElementById("backButton").addEventListener("click", () => {
-  window.location.href = "index.html";
-});
 
 window.onload = function () {
   navbar();
