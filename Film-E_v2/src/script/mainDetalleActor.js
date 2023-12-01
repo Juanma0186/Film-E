3706c649e074a70bb74c2f622660034b7de4f7ed
Juanma0186 <juanma0186@gmail.com>
2023-12-01 04:06:51 +0100

    DELETE boton volver atras

--- Film-E_v2/src/script/mainDetalleActor.js
+++ Film-E_v2/src/script/mainDetalleActor.js
@@ -6,7 +6,3 @@ window.onload = function () {
   verDetalleActor(ACTOR_API_URL);
   printMovies(ACTOR_MOVIE_CREDITS_API_URL, "cineList", true);
 };
-
-document.getElementById("backButton").addEventListener("click", () => {
-  window.history.back();
-});
