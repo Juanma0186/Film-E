75d85d82549e8762a8153ea36c7f5a3812d1d65e
Juanma0186 <juanma0186@gmail.com>
2023-12-01 06:22:44 +0100

    UPDATE SUSCRIPCION

--- Film-E_v2/src/script/suscripcion.js
+++ Film-E_v2/src/script/suscripcion.js
@@ -1,46 +1,10 @@
-console.clear();
+import { toTop } from "./toTop.js";
+import { navbar } from "./navbar.js";
 
-const cardsContainer = document.querySelector(".relative");
-// const cardsContainerInner = document.querySelector(".cards__inner");
-const cards = Array.from(document.querySelectorAll(".card"));
-const overlay = document.querySelector(".overlay");
-
-const applyOverlayMask = (e) => {
-  const overlayEl = e.currentTarget;
-  const x = e.pageX - cardsContainer.offsetLeft;
-  const y = e.pageY - cardsContainer.offsetTop;
-
-  overlayEl.style = `--opacity: 1; --x: ${x}px; --y:${y}px;`;
+window.onload = function () {
+  navbar();
 };
 
-const createOverlayCta = (overlayCard, ctaEl) => {
-  const overlayCta = document.createElement("div");
-  overlayCta.classList.add("cta");
-  overlayCta.textContent = ctaEl.textContent;
-  overlayCta.setAttribute("aria-hidden", true);
-  overlayCard.append(overlayCta);
+window.onscroll = function () {
+  toTop();
 };
-
-const observer = new ResizeObserver((entries) => {
-  entries.forEach((entry) => {
-    const cardIndex = cards.indexOf(entry.target);
-    const width = entry.borderBoxSize[0].inlineSize;
-    const height = entry.borderBoxSize[0].blockSize;
-
-    if (cardIndex >= 0) {
-      overlay.children[cardIndex].style.width = `${width}px`;
-      overlay.children[cardIndex].style.height = `${height}px`;
-    }
-  });
-});
-
-const initOverlayCard = (cardEl) => {
-  const overlayCard = document.createElement("div");
-  overlayCard.classList.add("card");
-  createOverlayCta(overlayCard, cardEl.lastElementChild);
-  overlay.append(overlayCard);
-  observer.observe(cardEl);
-};
-
-cards.forEach(initOverlayCard);
-document.body.addEventListener("pointermove", applyOverlayMask);
