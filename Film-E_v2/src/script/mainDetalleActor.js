3c90975557a2d03c6110bba928ac6884b3da3bd6
Juanma0186 <juanma0186@gmail.com>
2023-12-01 06:10:35 +0100

    UPDATE ACTOR

--- Film-E_v2/src/script/mainDetalleActor.js
+++ Film-E_v2/src/script/mainDetalleActor.js
@@ -1,8 +1,15 @@
 import { ACTOR_API_URL, ACTOR_MOVIE_CREDITS_API_URL } from "./config.js";
 import { printMovies } from "./printMovie.js";
 import { verDetalleActor } from "./verDetalleActor.js";
+import { navbar } from "./navbar.js";
+import { toTop } from "./toTop.js";
 
 window.onload = function () {
   verDetalleActor(ACTOR_API_URL);
   printMovies(ACTOR_MOVIE_CREDITS_API_URL, "cineList", true);
+  navbar();
+};
+
+window.onscroll = function () {
+  toTop();
 };
--- Film-E_v2/src/script/verDetalleActor.js
+++ Film-E_v2/src/script/verDetalleActor.js
@@ -1,5 +1,6 @@
 import { IMAGE_URL } from "./config.js";
 import { mostrarModal } from "./mostrarModal.js";
+import * as utilities from "./utilities.js";
 
 export function verDetalleActor(ACTOR_API_URL) {
   fetch(ACTOR_API_URL)
@@ -8,19 +9,21 @@ export function verDetalleActor(ACTOR_API_URL) {
       const actorDetails = document.getElementById("actorDetails");
       const imageUrl = actor.profile_path ? `${IMAGE_URL}${actor.profile_path}` : "img/default.webp";
       const actorName = actor.name || "Sin nombre";
-      const actorBiography = actor.biography || "Sin biografía";
-      const actorBirthday = actor.birthday || "Sin fecha de nacimiento";
+      const actorBirthday = utilities.formatDate(actor.birthday) || "Sin fecha de nacimiento";
+      const actorDeathday = `<p>Fallecido en: ${utilities.formatDate(actor.deathday)}</p>`;
       const actorPlaceOfBirth = actor.place_of_birth || "Sin lugar de nacimiento";
       const actorPopularity = actor.popularity || "Sin popularidad";
       actorDetails.innerHTML = `
-            <div class="flex flex-col md:flex-row">
-              <img id="actorImage" class="cursor-pointer w-full md:w-[400px] h-[400px] object-containt rounded-lg mb-5 md:mb-0 md:mr-8" src="${imageUrl}" alt="${actor.name}">
-              <div class="text-azul-100">
-                <h2 class="text-3xl font-bold mb-5">${actorName}</h2>
-                <p class="mb-4">${actorBiography}</p>
-                <p class="mb-4">Fecha de nacimiento: ${actorBirthday}</p>
-                <p class="mb-4">Lugar de nacimiento: ${actorPlaceOfBirth}</p>
-                <p class="mb-4">Popularidad: ${actorPopularity}</p>
+            <div class="flex flex-col md:flex-row gap-[1em]">
+              <div class="flex-[1-1-400px] flex items-center justify-center md:max-w-[400px]">
+                <img id="actorImage" class="cursor-pointer w-full h-full aspect-auto object-contain rounded-lg" src="${imageUrl}" alt="${actor.name}">
+              </div>
+              <div class="flex-[2] flex flex-col items-start gap-2 px-4 py-[2em] dark:text-blanco-500 text-xl">
+                  <h2 class="text-4xl font-bold dark:text-blanco-500 font-poppins">${actorName}<span class="text-gris-300 dark:text-gris-500 font-normal"> (${actorBirthday})</span></h2>
+                  <p> ${actorPlaceOfBirth}</p>
+                  <p><span class="underline">Bigrafía:</span><br/><span class="text-lg">${actor.biography ? actor.biography : "<b>[No disponible]</b>"}</span></p>
+                  ${actor.deathday ? actorDeathday : ""}
+                  <p">Popularidad: ${actorPopularity}</p>
               </div>
             </div>
             `;
